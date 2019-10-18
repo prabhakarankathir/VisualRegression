7e391446e482259e9c7b34e62c2884472712929a
prabharankathir <kprabhaaravind@gmail.com>
2019-10-18 23:20:20 +0530

    Initial commit

---
+++ tests/visualTest.spec.js
@@ -0,0 +1,12 @@
+describe('Verify the working of Visual Regression tests', function () {
+    
+    it('should load the url', () => {
+        browser.url('/');
+        console.log('Url loaded successfully');
+        browser.pause(2000);
+    });
+    
+     
+
+ 
+});
